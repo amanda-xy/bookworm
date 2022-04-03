d66bc827aa8c466ac0573239ce28f4702b02ce19
amanda-xy <erikssonmandaa@gmail.com>
2022-04-03 19:31:56 +0200

    first commit

---
+++ server/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+// allow cross-origin requests
+app.use(cors());
+
+mongoose.connect("mongodb+srv://amanda:3BwMFYcHW9WeWwbN@cluster0.qmivw.mongodb.net/bookworm?retryWrites=true&w=majority");
+mongoose.connection.once("open", () => {
+  console.log("connected to database");
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+app.listen(4000, () => {
+  console.log("Now listening for requests on port 4000");
+});
